9848fe8ccf62004e72033ac647eaf1af97cfa9cb
Mohit Dayma <mohitdayama1234@gmail.com>
2025-04-26 17:50:09 +0530

    fix typo in route

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
 // const isProtectedRoute = createRouteMatcher(['/user-profile']);
-const isPublicRoute = createRouteMatcher(['/', '/sign-in(*)', '/sign-up(*)']);
+const isPublicRoute = createRouteMatcher(['/', '/sign-in(.*)', '/sign-up(.*)']);
 
 export default clerkMiddleware(async (auth, req) => {
   // if (!isPublicRoute(req)) await auth.protect();
